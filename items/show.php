092824f43a8e16b5bf3cb89b69f49a0b53a0bcfb
Andy Meyer <ameyer24@gmail.com>
2014-11-09 16:43:18 -0600

    Updates to the items/show page

--- items/show.php
+++ items/show.php
@@ -1,27 +1,37 @@
 <?php echo head(array('title' => metadata($item, array('Dublin Core', 'Title')), 'bodyclass' => 'items show')); ?>
 
-
-
 <div id="primary">
-<<<<<<< HEAD
-
-=======
-<h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
->>>>>>> origin/master
+	<h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
     <!-- The following returns all of the files associated with an item. -->
     <?php if (metadata($item, 'has files')): ?>
     <div id="itemfiles" class="element">
         <div class="element-text"><?php echo files_for_item(array('imageSize' => 'fullsize')); ?></div>
     </div>
     <?php endif; ?>
-    <!-- The following prints a list of all tags associated with the item -->
+	
+	<ul class="item-pagination navigation">
+		<li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+		<li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+	</ul>
+  
+
+</div><!-- end primary -->
+
+<div id="secondary">
+
+    <!--  The following function prints all the the metadata associated with an item: Dublin Core, extra element sets, etc. See http://omeka.org/codex or the examples on items/browse for information on how to print only select metadata fields. -->
+    <?php echo all_element_texts($item); ?>
+
+    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+	<!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata($item, 'has tags')): ?>
     <div id="item-tags" class="element">
         <h2>Tags</h2>
         <div class="element-text tags"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif; ?>
-    <!--  The following function prints all the the metadata associated with an item: Dublin Core, extra element sets, etc. See http://omeka.org/codex or the examples on items/browse for information on how to print only select metadata fields. -->
+
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (get_collection_for_item()): ?>
         <div id="collection" class="element">
@@ -29,35 +39,19 @@
             <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
         </div>
     <?php endif; ?>
+
+    <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
         <h2>Citation</h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
 
-</div><!-- end primary -->
 
-<div id="secondary">
-<h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
+	
 
-
-
-
-    <?php echo all_element_texts($item); ?>
-
-    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
-
-
-
-
-
-    <!-- The following prints a citation for this item. -->
 
 </div><!-- end secondary -->
 
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
+
 
 <?php echo foot();
