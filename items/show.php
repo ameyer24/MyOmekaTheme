fc3aa75c6087faaf09a6a1a127c99a32ab027772
Andy Meyer <ameyer24@gmail.com>
2014-11-09 18:31:47 -0600

    Updated items/browse page
    Made the thumbnails larger

--- items/show.php
+++ items/show.php
@@ -1,13 +1,7 @@
 <?php echo head(array('title' => metadata($item, array('Dublin Core', 'Title')), 'bodyclass' => 'items show')); ?>
 
 <div id="primary">
-<<<<<<< HEAD
 	<h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
-=======
-
-<h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
-
->>>>>>> origin/master
     <!-- The following returns all of the files associated with an item. -->
     <?php if (metadata($item, 'has files')): ?>
     <div id="itemfiles" class="element">
